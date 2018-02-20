fc17b45053105b13c67dc8348d6fb5326dbeffcb
Gabriel Paladino <gabriel.paladino@stormsec.com.br>
2018-02-20 13:44:16 -0300

    challenge01.js

---
+++ js/challenge01.js
@@ -0,0 +1,13 @@
+function calc(str){
+
+}
+
+
+
+// Desafio: faça a função calc() retornar arrays conforme o padrão abaixo:
+
+console.log(calc(''));                      // returns []
+console.log(calc('AABBBBC'));               // returns ['2A', '4B', '1C']
+console.log(calc('AAABBBCCCDDDEEE'));       // returns ['3A', '3B', '3C', '3D', '3E']
+console.log(calc('AAABBBAABBCC'));          // returns ['3A', '3B', '2A', '2B', '2C']
+
